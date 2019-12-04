801843a83e34aa116e38004d3bca05e29619048e
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-04 12:11:34 -0800

    add and migrate new models for locations

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('locations/', include('locations.urls')),
+    path('', include('locations.urls')),
     path('admin/', admin.site.urls),
 ]
