4049a90669ba733ccd6cf4889f109ef5b676c588
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-04 13:17:18 -0800

    add detail, results, and vote views

--- locations/admin.py
+++ locations/admin.py
@@ -4,13 +4,11 @@ from .models import Question, Choice
 # Register your models here.
 class ChoiceInline(admin.TabularInline):
     model = Choice
-    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': [
-         'pub_date'], 'classes': ['collapse']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
--- locations/urls.py
+++ locations/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /locations/
     path('', views.index, name='index'),
+    # ex: /locations/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /locations/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /locations/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
