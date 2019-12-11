4def3952dd13f9c5b9fc09f3e22aa3280e3aea1a
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-11 14:50:44 -0800

    update rqs and rubric

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('locations.urls')),
+    path('locations/', include('locations.urls')),
     path('admin/', admin.site.urls),
 ]
