12bb2e1bf94be0b9a82f9042e6c3f8811cc7fe96
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-10 23:45:55 -0800

    add vertual environment adjust view

--- locations/views.py
+++ locations/views.py
@@ -4,6 +4,17 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the Locations index.")
 
+"""
+# from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+"""
+
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
