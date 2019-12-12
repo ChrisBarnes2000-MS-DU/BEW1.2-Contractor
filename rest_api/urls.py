54d28bf28fb8e353306df0fac2b7ae995c01be7b
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-12 11:52:38 -0800

    add views for api

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import PageList, PageDetail
+from rest_api.views import PageList, PageDetail
 
 urlpatterns = [
     path("", PageList.as_view(), name="Page_list"),
--- rest_api/views.py
+++ rest_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from locations.models import Page
+from rest_api.serializer import PageSerializer
+
+class PageList(generics.ListCreateAPIView):
+    queryset = Page.objects.all()
+    serializer_class = PageSerializer
+
+class PageDetail(generics.RetrieveDestroyAPIView):
+    queryset = Page.objects.all()
+    serializer_class = PageSerializer
