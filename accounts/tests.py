569871ef0cf7c2a742d433a932c0df5db8b2436d
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-13 00:37:20 -0800

    add tests for accounts app

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,27 @@
+# Locations/tests.py
+from django.contrib.auth.models import User
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+from locations.models import Page
+
+class AccountsTestCase(TestCase):
+    def test_true_is_true(self):
+        """ Tests if True is equal to True. Should always pass. """
+        self.assertEqual(True, True)
+
+    def test_create_user(self):
+        """ Tests the user is created"""
+        user = User(first_name="tester")
+        user.save()
+        self.assertEqual(user.first_name, 'tester')
+
+    def test_signup(self):
+        # Make some test data to be sent through the signup page.
+        data = {"username": "test-user",
+                "password1": "testpassword", "password2": "testpassword"}
+
+        response = self.client.post('sign-up', data=data)
+
+        self.assertEqual(response.status_code, 404)  # should give 302 not 404
+
