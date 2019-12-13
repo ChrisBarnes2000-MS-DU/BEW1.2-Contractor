956bcc918ebaff78265f846d6c8988cead65d39f
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-12 23:15:53 -0800

    update reqs and settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'locational-trivia.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'locational-trivia.herokuapp.com']
 
 DEFAULT_LOGOUT_URL = '/'
 
