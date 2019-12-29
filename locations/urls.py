c3a2cc122fd4619b1f851ad47a2ca78f8c7fbc06
Christopher Barnes <cbarnes7404@yahoo.com>
2019-12-28 19:36:16 -0800

    adjust form layout

--- locations/urls.py
+++ locations/urls.py
@@ -5,17 +5,21 @@ from django.conf.urls.static import static
 from locations.views import PageListView, PageCreateView, PageDetailView, PageEditView, detail, results, vote
 
 urlpatterns = [
+    # ex: /locations
     path('', PageListView.as_view(), name='list-page'),
-    path('new/', PageCreateView.as_view(), name='create-page'),
-    path('<str:slug>/', PageDetailView.as_view(), name='details-page'),
-    path('<str:slug>/edit/', PageEditView.as_view(), name='edit-page'),
 
-    # ex: /locations/
-    # path('', index, name='index'),
     # ex: /locations/5/
     path('<int:question_id>/', detail, name='detail'),
     # ex: /locations/5/results/
     path('<int:question_id>/results/', results, name='results'),
     # ex: /locations/5/vote/
     path('<int:question_id>/vote/', vote, name='vote'),
+
+    # ex: /locations/new/
+    path('new/', PageCreateView.as_view(), name='create-page'),
+    # ex: /locations/san-francisco/
+    path('<str:slug>/', PageDetailView.as_view(), name='details-page'),
+    # ex: /locations/edit/
+    path('<str:slug>/edit/', PageEditView.as_view(), name='edit-page'),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
