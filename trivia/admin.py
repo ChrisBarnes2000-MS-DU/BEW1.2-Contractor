4ab06c552fdcc47a1e6d640e390f1a0659338e9b
Christopher Barnes <cbarnes7404@yahoo.com>
2020-01-08 12:03:17 -0800

    fixed db table and quiz rendering in admin

--- trivia/admin.py
+++ trivia/admin.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
-from trivia.models import Exam, Question
-
-class ExamAdmin(admin.ModelAdmin):
-    """ Show helpful fields on the changelist page. """
-    list_display = ('title', 'author', 'created', 'modified')
-
+from trivia.models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
     """ Show helpful fields on the changelist page. """
     list_display = ('content', 'author', 'created', 'modified')
 
-
-admin.site.register(Exam, ExamAdmin)
 admin.site.register(Question, QuestionAdmin)
--- trivia/models.py
+++ trivia/models.py
@@ -2,26 +2,29 @@ import datetime
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
-
-class Exam(models.Model):
-    """ Represents a quiz/exam. """
-    objects = models.Manager()
-    created = models.DateTimeField(auto_now_add=True, help_text="The date&time this exam was created. Automatically generated when the model saves.")
-    modified = models.DateTimeField(auto_now=True, help_text="The date&time this exam was updated. Automatically generated when the model updates.")
-    title = models.CharField(max_length=settings.PAGE_TITLE_MAX_LENGTH, unique=True, default="Title of your exam.")
-    author = models.ForeignKey(User, on_delete=models.PROTECT, help_text="The user that posted this exam.")
-
-    def __str__(self):
-        return self.title
+from locations.models import Page
 
 class Question(models.Model):
     """ Represents a question. """
     objects = models.Manager()
-    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Page, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.PROTECT, help_text="The user that posted this question.")
     created = models.DateTimeField(auto_now_add=True, help_text="The date&time this question was created. Automatically generated when the model saves.")
     modified = models.DateTimeField(auto_now=True, help_text="The date&time this question was updated. Automatically generated when the model updates.")
     content = models.TextField(default="Write the content of your question here.")
-    author = models.ForeignKey(User, on_delete=models.PROTECT, help_text="The user that posted this exam.")
+
+    def __str__(self):
+        return self.content
+
+class Choice(models.Model):
+    """ Represents a question. """
+    objects = models.Manager()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, help_text="The date&time this question was created. Automatically generated when the model saves.")
+    modified = models.DateTimeField(auto_now=True, help_text="The date&time this question was updated. Automatically generated when the model updates.")
+    points = models.IntegerField(default=10, help_text="number of points given for getting the correct answer to this question")
+    content = models.TextField(default="Write the content of your question here.")
+    correct = models.BooleanField(default=False, help_text="Is this the correct answer")
 
     def __str__(self):
         return self.content
