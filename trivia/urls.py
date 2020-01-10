6894ab0f20c69794953d9e5d016ba81eeb0cb220
Christopher Barnes <cbarnes7404@yahoo.com>
2020-01-10 12:39:48 -0800

    adjust views

--- trivia/urls.py
+++ trivia/urls.py
@@ -3,9 +3,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from trivia.views import trivia
+# , grade
 
 urlpatterns = [
     # ex: /trivia/san-francisco/
     path('<str:slug>/', trivia, name='trivia'),
+    # path('<str:slug>/', grade, name='grade')
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- trivia/views.py
+++ trivia/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from trivia.models import Question, Choice
 from locations.models import Page
 from django.urls import reverse
+from django.views import generic
 import requests
 import json
 
@@ -11,6 +12,7 @@ def trivia(request, slug):
     template_name = 'trivia/game.html'
 
     # Generate counts of some of the main objects
+    # questions = Question.objects.filter(quiz__slug=slug)
     questions = Question.objects.filter(quiz__slug=slug)
     choices = Choice.objects.filter(question__quiz__slug=slug).order_by('created')
     num_questions = questions.count()
@@ -30,28 +32,31 @@ def trivia(request, slug):
     return render(request, template_name, context=context)
 
 
-# def trivia(request, slug):
-#     template_name = 'trivia/game.html'
+# def game(request, slug):
 #     questions = Question.objects.filter(quiz__slug=slug)
-#     total_points = sum([question.points for question in questions]) 
-#     num_questions = questions.count()
 #     choices = Choice.objects.filter(question__quiz__slug=slug).order_by('created')
-
+#     total_points = sum([question.points for question in questions])
+#     num_questions = questions.count()
+#     # template_name = 'scoreboard/score_board.html'
+#     template_name = 'trivia/game.html'
 #     try:
-#         selected_choices = choices.get(request.POST[question])
+#         selected_choices = questions.get(request.POST['choice'])
 #     except (KeyError, Choice.DoesNotExist):
 #         context = {
 #             'slug': slug,
 #             'total_points': total_points,
 #             'num_questions': num_questions,
-#             'questions': questions,
+#             'questions': questions[:1],
 #             'choices': choices,
 #             'error_message': "You didn't select a choice.",
 #         }
 #         return render(request, template_name, context=context)
 #     else:
-#         # if selected_choices:
-#         # Always return an HttpResponseRedirect after successfully dealing
-#         # with POST data. This prevents data from being posted twice if a
-#         # user hits the Back button.
-#         return HttpResponseRedirect(reverse('highscore'))
+#         if selected_choices.correct:
+            
+#             # Always return an HttpResponseRedirect after successfully dealing
+#             # with POST data. This prevents data from being posted twice if a
+#             # user hits the Back button.
+#             return HttpResponseRedirect(reverse('highscore'))
+
+
